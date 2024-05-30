3c537616b85734517de3f64286f57f9dc014e09d
Crowdbotics <team@crowdbotics.com>
2024-05-30 14:53:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "speech-therapy-48178",
+  url: "https://speech-therapy-48178.botics.co",
+  api: "https://speech-therapy-48178.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
